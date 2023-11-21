3d0c88385544256fc48d1dec4f28f6c9efa42c1a
Sathikbasha12345 <122858052+Sathikbasha12345@users.noreply.github.com>
2023-11-21 21:24:12 +0530

    added new files

--- repository/UserLoginDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package saaspe.security.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import saaspe.security.entity.UserLoginDetails;
-
-public interface UserLoginDetailsRepository extends JpaRepository<UserLoginDetails, String> {
-
-}
